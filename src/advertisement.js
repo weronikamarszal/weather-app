358474d683a41bb18a5c92708155d74f410bc7cb
weronikamarszal <m.weero@gmail.com>
2020-11-12 16:14:24 +0100

    advertisements image+desc

--- src/advertisement.js
+++ src/advertisement.js
@@ -23,20 +23,23 @@ const Tag = {
 }
 
 const advertisements = [
-    new Advertisement('Ad1', 1, 'desc1', null, null, Tag.COLD),
-    new Advertisement('Ad2', 2, 'desc2', null, null, Tag.HOT),
-    new Advertisement('Ad3', 3, 'desc3', null, null, Tag.RAIN),
-    new Advertisement('Ad4', 4, 'desc4', null, null, Tag.RAIN),
-    new Advertisement('Ad5', 5, 'desc5', null, null, Tag.RAIN),
-    new Advertisement('Ad6', 6, 'desc6', null, null, Tag.RAIN),
-    new Advertisement('Ad7', 7, 'desc7', null, null, Tag.RAIN),
-    new Advertisement('Ad8', 8, 'desc8', null, null, Tag.RAIN),
-    new Advertisement('Ad9', 9, 'desc9', null, null, Tag.RAIN),
-    new Advertisement('Ad10', 10, 'desc10', null, null, Tag.RAIN),
+    new Advertisement('Ad1', 1, 'zimno? kup sweter', "./img/ads/cold.jpeg", null, Tag.COLD),
+    new Advertisement('Ad2', 2, 'ciepło? zjedz loda', "./img/ads/hot.jpeg", null, Tag.HOT),
+    new Advertisement('Ad3', 3, 'zimno? brrrrrrr', "./img/ads/cold.jpeg", null, Tag.COLD),
+    new Advertisement('Ad4', 4, 'ale mróz!', "./img/ads/cold.jpeg", null, Tag.COLD),
+    new Advertisement('Ad5', 5, 'kap kap kap', "./img/ads/rain.jpeg", null, Tag.RAIN),
+    new Advertisement('Ad6', 6, 'pada? kup parasol', "./img/ads/rain.jpeg", null, Tag.RAIN),
+    new Advertisement('Ad7', 7, 'zapraszam na lody', "./img/ads/hot.jpeg", null, Tag.HOT),
+    new Advertisement('Ad8', 8, 'daj się ochłodzić', "./img/ads/hot.jpeg", null, Tag.HOT),
+    new Advertisement('Ad9', 9, 'deszcz za oknami', "./img/ads/rain.jpeg", null, Tag.RAIN),
+    new Advertisement('Ad10', 10, 'zmokłeś?', "./img/ads/rain.jpeg", null, Tag.RAIN),
 ];
 
 function getTag(weather) {
     let tags = [];
+    if (!weather) {
+        return;
+    }
     if (weather.main.temp <= 10) {
         tags.push(Tag.COLD);
     }
@@ -48,7 +51,6 @@ function getTag(weather) {
     }
 
     let randomIndex = Math.floor(Math.random() * tags.length);
-    console.log(tags[randomIndex])
     return tags[randomIndex];
 
 }
@@ -61,11 +63,29 @@ function ad(tag) {
     return filtered[randomIndex];
 }
 
-function displayAd(weather) {
-    let htmlElement = document.getElementById("advertisement");
-    htmlElement.innerText = ad(getTag(weather)).name;
-    console.log(weather);
+function createAdvertisementHTML(advertisement) {
+    let element = document.createElement("div");
+    let elementInner = document.querySelector("#advertisementTemplate").content.cloneNode(true);
+    element.append(elementInner);
+
+    let image = element.querySelector(".image");
+    image.src = advertisement.image;
+    let desc = element.querySelector(".desc");
+    desc.innerText = advertisement.description;
+    let title = element.querySelector(".title");
+    title.innerText = advertisement.name;
+
+    return element;
 }
 
-displayAd();
+function displayAd(weather) {
+    let adElements = document.querySelectorAll(".advertisement");
+    adElements.forEach((i) => {
+        if (i.children[0]) {
+            i.children[0].remove();
+        }
+        i.append(createAdvertisementHTML(ad(getTag(weather))));
+
+    })
+}
 
