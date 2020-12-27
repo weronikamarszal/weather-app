cc4315af3f914b58a0c0823bb7f690edefb56af8
Danylo Arbuzov <arbuzdan@fit.cvut.cz>
2020-12-27 23:21:32 +0100

    working on userManaging panel

--- js/userManaging.js
+++ js/userManaging.js
@@ -1,3 +1,16 @@
+// let $table = $('#table')
+let myTab = document.getElementById('table');
+
 function clickTableRaw() {
-    alert("works");
+//     // alert(JSON.stringify($table.bootstrapTable('getData')));
+//     let objCells = myTab.rows.item(1).cells;
+//     alert(objCells.item(1).innerHTML);
 }
+
+$(document).ready(function() {
+    $(document).on("click", "#table tbody tr", function() {
+        var data = $(this).closest('tr').attr('id');
+        alert(data);
+    });
+});
+
