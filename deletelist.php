c21f2c5eba80693931d630fe17bc854075902041
Danylo Arbuzov <arbuzdan@fit.cvut.cz>
2021-01-17 21:22:17 +0100

    clearing the code

--- deletelist.php
+++ deletelist.php
@@ -5,12 +5,6 @@ $id = $_GET['id'];
 
 $query = "DELETE FROM users WHERE userId = '$id'";
 $result = mysqli_query($conn, $query) or die( mysqli_error($conn) );
-//try {
-//    $query=$conn-> prepare("DELETE * FROM users WHERE id=:id");
-//    $query->execute(array(':id' => $_GET['id']));
-//}
-//catch(Exception $e){
-//    throw new Exception($e->getMessage());
-//}
+
 
 header("Location: http://localhost:63342/weather-app/userManaging.php");
