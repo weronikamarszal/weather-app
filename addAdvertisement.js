86611280a5cef8f365dc89cf65834dd6d5ffc13f
weronikamarszal <wero.marszal@gmail.com>
2021-01-21 17:19:00 +0100

    edit adverts

--- addAdvertisement.js
+++ addAdvertisement.js
@@ -1,11 +1,38 @@
-document.getElementById("addAdvertisementForm").addEventListener('submit', (event) => {
-    event.preventDefault()
-    const formValue = new FormData(event.target)
-
-    fetch('/weather-app/php/addAdvertisement.php', { method: 'POST', body: formValue })
+function createAd(formValue) {
+    fetch('/weather-app/php/addAdvertisement.php', {method: 'POST', body: formValue})
         .then(data => data.text())
         .then(data => {
             location.replace('/weather-app/advertPanel.html')
         })
+}
 
+function editAd(id, formValue) {
+    formValue.append('id', id)
+    fetch(`/weather-app/php/addAdvertisement.php?id=${id}`, {method: 'POST', body: formValue})
+        .then(data => data.text())
+        .then(data => {
+            location.replace('/weather-app/advertPanel.html')
+        })
+}
+
+let id = null;
+document.getElementById("addAdvertisementForm").addEventListener('submit', (event) => {
+    event.preventDefault()
+    const formValue = new FormData(event.target)
+    if (id !== null) {
+        editAd(id, formValue)
+    } else {
+        createAd(formValue);
+    }
 });
+new URLSearchParams(location.search).forEach((e, x) => {
+    let element = document.getElementById(x);
+    if (element !== null) {
+        element.value = e;
+    }
+    if (x === "id") {
+        id = e;
+    }
+
+})
+
