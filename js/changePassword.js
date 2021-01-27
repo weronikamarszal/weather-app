6dbadac893a67035ba4677bf53e3d5e0ec46bd0e
Danylo Arbuzov <arbuzdan@fit.cvut.cz>
2021-01-27 20:07:11 +0100

    user panel twoje dane done

--- js/changePassword.js
+++ js/changePassword.js
@@ -12,10 +12,24 @@ function showPassword() {
     }
 }
 
+function funcSuccess() {
+    err.innerText = "Hasło zostało zmienione";
+    err.style.color = "green";
+    x.value = "";
+    y.value = "";
+    return false;
+}
+
 function changePassword() {
     if (x.value !== "" || y.value !== "") {
         if (x.value === y.value) {
-
+            $.ajax({
+                url: "http://localhost:63342/weather-app/changePassword.php?_ijt=rd97bab58eo5f7kdbfpjnvp76m",
+                type: "POST",
+                data: ({password: x.value}),
+                dataType: "text",
+                //success: funcSuccess
+            });
             err.innerText = "Hasło zostało zmienione";
             err.style.color = "green";
             x.value = "";
